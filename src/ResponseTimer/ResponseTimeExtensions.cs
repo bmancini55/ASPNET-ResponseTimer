e446272336db675a75ad3f6bc51059b12180603a
Brian Mancini <bmancini@gmail.com>
2015-05-20 09:01:19 -0400

    Initial commit for source

---
+++ src/ResponseTimer/ResponseTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using ResponseTimer;
+
+namespace Microsoft.AspNet.Builder
+{
+    public static class ResponseTimeExtensions
+    {
+        public static IApplicationBuilder UseResponseTimer(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ResponseTimerMiddleware>();
+        }
+    }
+}
---
+++ src/ResponseTimer/ResponseTimerMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.Builder;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using System.Diagnostics;
+
+namespace ResponseTimer
+{
+    public class ResponseTimerMiddleware
+    {
+        private readonly RequestDelegate next;        
+
+        public ResponseTimerMiddleware(RequestDelegate next, IHostingEnvironment hostingEnv)
+        {
+            this.next = next;
+        }
+
+        public Task Invoke(HttpContext context)
+        {            
+            var sw = Stopwatch.StartNew();
+            context.Response.OnSendingHeaders((state) =>
+            {                
+                sw.Stop();
+                context.Response.Headers.Add("X-Response-Time", new string[] { sw.ElapsedMilliseconds.ToString() + "ms" });
+            }, null);
+            return next(context);
+        }
+
+    }
+}
